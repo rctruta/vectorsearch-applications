b99f48195a63486313e1ce90b5fdb540452b197f
rctruta <ramona.truta@gmail.com>
2024-05-20 23:24:01 -0400

    Week 2 submission

---
+++ notebooks/test.py
@@ -0,0 +1,5 @@
+from weaviate.collections.classes.internal import (MetadataReturn, QueryReturn,
+                                                   MetadataQuery)
+
+print(MetadataReturn)
+print('if you made it this far, Good Job!')
